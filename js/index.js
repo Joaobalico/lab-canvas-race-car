fbbab9eeca8ee45816ca668c558eabe4a8c3d88d
Joao Balico <joaobalico12@hotmail.com>
2022-01-28 09:06:16 +0000

    Incomplete lab

--- js/index.js
+++ js/index.js
@@ -1,7 +1,27 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+
+function car() {
+  const carImg = new Image();
+
+}
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
     startGame();
   };
 
-  function startGame() {}
-};
+  function startGame() {
+    const img = new Image();
+    img.addEventListener('load', () => {
+      ctx.drawImage(img, 0, 0);
+    })
+    img.src = './images/road.png';
+    const carImg = new Image();
+    carImg.addEventListener('load', () => {
+      ctx.drawImage(carImg, 60, 120);
+    })
+    carImg.src = './images/car.png'
+  };
+}
